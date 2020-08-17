8fd0f1aecf47e8320a387cae5af2108828aacc59
Andre Baltieri <andrebaltieri@hotmail.com>
2020-08-17 19:16:30 -0300

    Finalizado demo

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import TodoList from './components/TodoList';
+import TodoContext from './contexts/TodoContext'
+import AddTodo from './components/AddTodo';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <TodoContext>
+      <Router>
+        <Navbar></Navbar>
+        <br />
+        <div className="uk-container">
+          <Switch>
+            <Route path="/create">
+              <AddTodo></AddTodo>
+            </Route>
+            <Route path="/">
+              <h4>Minha lista de tarefas</h4>
+              <TodoList></TodoList>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </TodoContext>
+  );
+}
+
+export default App;
